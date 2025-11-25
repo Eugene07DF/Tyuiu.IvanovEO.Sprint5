4e05102c0ad2895013d2e3edaf11a339a582340a
Евгений Иванов <Tyuiu.IvanovEO@outlook.com>
2025-11-26 01:27:37 +0500

    Task7

---
+++ Tyuiu.IvanovEO.Sprint5.Task7.V11.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.IvanovEO.Sprint5.Task7.V11.Lib
+{
+    public class DataService : ISprint5Task7V11
+    {
+        public string LoadDataAndSave(string path)
+        {
+            return "1";
+        }
+    }
+}
