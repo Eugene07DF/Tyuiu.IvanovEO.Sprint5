76112247ea16576552277ba3b981786c1aa22110
Евгений Иванов <Tyuiu.IvanovEO@outlook.com>
2025-11-26 01:06:49 +0500

    Task4

--- Tyuiu.IvanovEO.Sprint5.Task4.V30.Lib/Class1.cs
+++ Tyuiu.IvanovEO.Sprint5.Task4.V30.Lib/Class1.cs
@@ -12,15 +12,15 @@ namespace Tyuiu.IvanovEO.Sprint5.Task4.V30.Lib
                 string xStr = File.ReadAllText(path);
                 double x = double.Parse(xStr, CultureInfo.InvariantCulture);
 
-                // Вычисляем значение по формуле: y = (0.3 * tg(x)) + 2.03 * x
-                double result = (0.3 * Math.Tan(x)) + (2.03 * x);
+                // Вычисляем значение по формуле: y = (x³ - tg(x) + 2.03x)
+                double result = (Math.Pow(x, 3) - Math.Tan(x) + 2.03 * x);
 
                 // Округляем до трёх знаков после запятой
                 double roundedResult = Math.Round(result, 3);
 
                 // Выводим на консоль
                 Console.WriteLine($"x = {x}");
-                Console.WriteLine($"y = (0.3 * tg({x})) + 2.03 * {x} = {roundedResult}");
+                Console.WriteLine($"y = ({x}³ - tg({x}) + 2.03 * {x}) = {roundedResult}");
 
                 return roundedResult;
             }
@@ -31,4 +31,5 @@ namespace Tyuiu.IvanovEO.Sprint5.Task4.V30.Lib
             }
         }
     }
+    
 }
