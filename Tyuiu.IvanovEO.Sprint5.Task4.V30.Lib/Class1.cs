a5a6f0055fe31acd273026603ba7d3dba8708665
Евгений Иванов <Tyuiu.IvanovEO@outlook.com>
2025-11-26 01:05:10 +0500

    Task4

---
+++ Tyuiu.IvanovEO.Sprint5.Task4.V30.Lib/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.IvanovEO.Sprint5.Task4.V30.Lib
+{
+    public class Class1 : ISprint5Task4V30
+    {
+        public double LoadFromDataFile(string path)
+        {
+            try
+            {
+                // Читаем значение X из файла
+                string xStr = File.ReadAllText(path);
+                double x = double.Parse(xStr, CultureInfo.InvariantCulture);
+
+                // Вычисляем значение по формуле: y = (0.3 * tg(x)) + 2.03 * x
+                double result = (0.3 * Math.Tan(x)) + (2.03 * x);
+
+                // Округляем до трёх знаков после запятой
+                double roundedResult = Math.Round(result, 3);
+
+                // Выводим на консоль
+                Console.WriteLine($"x = {x}");
+                Console.WriteLine($"y = (0.3 * tg({x})) + 2.03 * {x} = {roundedResult}");
+
+                return roundedResult;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                return 0;
+            }
+        }
+    }
+}
---
+++ Tyuiu.IvanovEO.Sprint5.Task4.V30.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.IvanovEO.Sprint5.Task4.V30.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
