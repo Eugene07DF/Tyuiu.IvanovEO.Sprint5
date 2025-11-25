24026ba944b8c70adea7cf55ff175f2a9d31cff0
Евгений Иванов <Tyuiu.IvanovEO@outlook.com>
2025-11-26 00:32:34 +0500

    Task3

---
+++ Tyuiu.IvanovEO.Sprint5.Task3.V22.Lib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.IvanovEO.Sprint5.Task3.V22
+{
+    public class Class1 : ISprint5Task3V22
+    {
+        public string SaveToFileTextData(int x)
+        {
+            return "1";
+        }
+    }
+}
