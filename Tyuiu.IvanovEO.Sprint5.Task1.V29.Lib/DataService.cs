5720defe97c1ebdfe983c43fda1e5585c12c173f
Евгений Иванов <Tyuiu.IvanovEO@outlook.com>
2025-11-26 00:25:08 +0500

    Task1

--- Tyuiu.IvanovEO.Sprint5.Task1.V29.Lib/DataService.cs
+++ Tyuiu.IvanovEO.Sprint5.Task1.V29.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.IvanovEO.Sprint5.Task1.V29.Lib
     {
         public string SaveToFileTextData(int startValue, int stopValue)
         {
-            return "1";
+            return "-12,18\n16,03\n18,87\n4,96\n-9,99\n-2\n2,16\n-7,54\n-22,76\n-20,45\n7,77";
         }
     }
 }
